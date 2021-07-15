3922060663b29079922e9884d6171549479c569c
efburgosr <efburgosr@unal.edu.co>
2021-07-15 10:12:02 -0500

    Actualización de path api rest

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://visiturroom-env.eba-vpsjmfhz.us-east-2.elasticbeanstalk.com'
+  //apiBaseUrl: 'http://visiturroom-env.eba-vpsjmfhz.us-east-2.elasticbeanstalk.com'
+  apiBaseUrl: 'http://localhost:5000'
+  
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://visiturroom-env.eba-vpsjmfhz.us-east-2.elasticbeanstalk.com'
+  //apiBaseUrl: 'http://visiturroom-env.eba-vpsjmfhz.us-east-2.elasticbeanstalk.com'
+  apiBaseUrl: 'http://localhost:5000'
 };
 
 /*
